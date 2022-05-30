4592a4d6a41cdc9baabaef66d0ff41176bc5fa58
Tomoyuki Sakurai <y@trombik.org>
2022-05-30 22:09:05 +0700

    bugfix: clang-format

--- examples/hello_world/main/main.c
+++ examples/hello_world/main/main.c
@@ -9,8 +9,8 @@ void
 app_main(void)
 {
 
-	esp_hass_hello_world();
-	while (1) {
-		vTaskDelay(1000 / portTICK_PERIOD_MS);
+    esp_hass_hello_world();
+    while (1) {
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
 }
