3b8baf6aa05e663d26f95a532bc4b645bfdd404d
Tomoyuki Sakurai <y@trombik.org>
2022-05-30 22:11:38 +0700

    bugfix: clang-format

--- examples/hello_world/main/main.c
+++ examples/hello_world/main/main.c
@@ -1,16 +1,15 @@
-#include <stdio.h>
-
 #include <esp_hass.h>
 #include <esp_log.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
+#include <stdio.h>
 
 void
 app_main(void)
 {
 
-    esp_hass_hello_world();
-    while (1) {
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
+	esp_hass_hello_world();
+	while (1) {
+		vTaskDelay(1000 / portTICK_PERIOD_MS);
+	}
 }
