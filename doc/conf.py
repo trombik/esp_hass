e61ad4d61def77d2236bd9ce20a3a9e69fab3832
Tomoyuki Sakurai <y@trombik.org>
2022-06-06 18:53:41 +0700

    ci: fix wrong python usage

--- doc/conf.py
+++ doc/conf.py
@@ -18,7 +18,7 @@ import sys
 import clang.cindex
 if os.uname().sysname == 'FreeBSD':
   clang.cindex.Config.set_library_file('/usr/local/llvm13/lib/libclang.so.13')
-if os.environ['CLANG_LIB_DIR']:
+if 'CLANG_LIB_DIR' in os.environ:
   clang.cindex.Config.set_library_path(os.environ['CLANG_LIB_DIR'])
 
 # -- Project information -----------------------------------------------------
